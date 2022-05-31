31fce93384a2109b7f92ba5529028e86b3b46fe6
NazarLukashik <nazarlykashik@gmail.com>
2022-05-31 21:10:03 +0300

    one bad in text fields

--- Colour mixer/ViewController.swift
+++ Colour mixer/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mainLabel: UIView!
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
@@ -18,10 +21,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     
-    @IBOutlet weak var redTextField: UITextField!
-    @IBOutlet weak var greenTextField: UITextField!
-    @IBOutlet weak var blueTextField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // colour labeles
@@ -29,11 +28,11 @@ class ViewController: UIViewController {
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
         
-//        setValueForLabel()
-//
-//        addDoneButtonTo(redTextField)
-//        addDoneButtonTo(greenTextField)
-//        addDoneButtonTo(blueTextField)
+
+
+        addDoneButtonTo(redTextField)
+        addDoneButtonTo(greenTextField)
+        addDoneButtonTo(blueTextField)
         
         redLabel.text = String(format:"%.1f",redSlider.value)
         greenLabel.text = String(format:"%.1f",greenSlider.value)
@@ -65,13 +64,6 @@ class ViewController: UIViewController {
                           blueSlider: blueSlider.value)
     }
     
-    @IBAction func redValue(_ sender: UISlider) {
-       // redTextField.text = String(from: 0)
-    }
-    @IBAction func greenValue(_ sender: Any) {
-    }
-    @IBAction func blueValue(_ sender: Any) {
-    }
     func backGroundSliders(redSlider: Float,
                            greenSlider:Float,
                            blueSlider:Float){
@@ -81,9 +73,72 @@ class ViewController: UIViewController {
                                              alpha: 1)
     }
 }
+extension ViewController: UITextFieldDelegate{
+    //скрываем клавиатуру на done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
+    //скрытие клавиатуры по тапу на экран за пределами тест вью
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true) //скрывает клавиатуру, вызванную для любого объекта
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        guard let text = textField.text else { return }
+        
+        if let currentValue = Float(text) {
+            
+            switch textField.tag {
+            case 0: redSlider.value = currentValue
+            case 1: greenSlider.value = currentValue
+            case 2: blueSlider.value = currentValue
+            default: break
+            }
+            backGroundSliders(redSlider: redSlider.value,
+                              greenSlider: greenSlider.value,
+                              blueSlider: blueSlider.value)
+            redLabel.text = String(format:"%.1f",redSlider.value)
+            greenLabel.text = String(format:"%.1f",greenSlider.value)
+            blueLabel.text = String(format:"%.1f",blueSlider.value)
+        }else{
+            showAlert(title: "Wrong format!", message: "Please enter correct value")
+        }
+     }
+}
 
+extension ViewController {
+    // отображаем кнопку готово в клавиатуре
+    private func addDoneButtonTo(_ textField: UITextField) {
+        
+        let keyboardToolbar = UIToolbar()
+        textField.inputAccessoryView = keyboardToolbar
+        keyboardToolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title:"Done",
+                                         style: .done,
+                                         target: self,
+                                         action: #selector(didTapDone))
+        
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil,
+                                            action: nil)
+        
+        
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
     
-
-
-
+    @objc private func didTapDone() {
+        self.view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
